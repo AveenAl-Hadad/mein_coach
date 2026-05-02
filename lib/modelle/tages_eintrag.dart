f7ec43d949428fd976e98c384668a8fd87dc42ca
aveen alhadad <aveen_alhadad@yahoo.com>
2026-05-02 14:17:17 +0200

    Feature: Datum zum TagesEintrag hinzugefügt

--- lib/modelle/tages_eintrag.dart
+++ lib/modelle/tages_eintrag.dart
@@ -1,22 +1,25 @@
-/// Datenmodell für einen Tag.
-/// Diese Klasse speichert Gewicht, Wasser, Schritte und Mahlzeiten.
 class TagesEintrag {
+  String datum; // NEU
   double gewicht;
   int wasser;
   int schritte;
   List<String> mahlzeiten;
 
   TagesEintrag({
+    required this.datum,
     required this.gewicht,
     required this.wasser,
     required this.schritte,
     required this.mahlzeiten,
   });
 
-  /// Erstellt einen Standard-Eintrag,
-  /// falls noch keine gespeicherten Daten existieren.
-  factory TagesEintrag.standard() {
+  factory TagesEintrag.heute() {
+    final heute = DateTime.now();
+    final datumText =
+        "${heute.year}-${heute.month}-${heute.day}";
+
     return TagesEintrag(
+      datum: datumText,
       gewicht: 80.0,
       wasser: 0,
       schritte: 0,
