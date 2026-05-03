6efc07488389cc27f5eb9fa2c46a6aeed9639728
aveen alhadad <aveen_alhadad@yahoo.com>
2026-05-03 13:38:29 +0200

    Feature: TagesProvider in App eingebunden

--- lib/app/meine_app.dart
+++ lib/app/meine_app.dart
@@ -1,21 +1,28 @@
 import 'package:flutter/material.dart';
 import '../seiten/haupt_navigation.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../provider/tages_provider.dart';
+import '../seiten/haupt_navigation.dart';
 
 /// Hauptklasse der App.
-/// Hier werden Design, Titel und Startseite festgelegt.
+/// Hier werden Provider, Design und Startseite festgelegt.
 class MeineApp extends StatelessWidget {
   const MeineApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Mein Coach',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorSchemeSeed: Colors.green,
-        useMaterial3: true,
+    return ChangeNotifierProvider(
+      create: (_) => TagesProvider()..datenLaden(),
+      child: MaterialApp(
+        title: 'Mein Coach',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorSchemeSeed: Colors.green,
+          useMaterial3: true,
+        ),
+        home: const HauptNavigation(),
       ),
-      home: const HauptNavigation(),
     );
   }
 }
