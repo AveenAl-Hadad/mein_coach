c7b053a62ed394a8bbee6904a2af2815a3129949
aveen alhadad <aveen_alhadad@yahoo.com>
2026-05-05 14:53:36 +0200

    Feature: Erinnerungen beim App Start automatisch aktiviere

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,24 @@ import 'package:flutter/material.dart';
 
 import 'app/meine_app.dart';
 import 'services/erinnerung_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   final erinnerungService = ErinnerungService();
   await erinnerungService.starten();
+
+  // 🔥 Einstellungen laden
+  final speicher = await SharedPreferences.getInstance();
+  final erinnerungAktiv =
+      speicher.getBool('wasser_erinnerung_aktiv') ?? false;
+
+  // 🔔 Wenn aktiv → automatisch starten
+  if (erinnerungAktiv) {
+    await erinnerungService.wasserErinnerungenZuEchtenZeitenStarten();
+  }
 
   runApp(const MeineApp());
 }
